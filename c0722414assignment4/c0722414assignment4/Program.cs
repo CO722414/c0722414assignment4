ce2d81db6a401d961318f0bd2d29ac29f34936e3
Ramandeep Kaur <722414@student.tor.loc>
2019-04-11 17:58:10 -0400

    commit

---
+++ c0722414assignment4/c0722414assignment4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0730108_Assignment_4
+{
+    class Program
+    {
+        ArrayList Beowulf;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+
+        }
+        public void Run() { this.ReadTextFiles(); }
+
+        public void ReadTextFiles()
+        {
+            //Read file using StreamReader.Reads file line by line 
+            using (StreamReader file = new StreamReader("U:/Users/722414/c0722414assignment4Beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+
+        }
+
+    }
+}
